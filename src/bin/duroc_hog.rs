be711d6e9e070ab60db53034f995ad3206be8bda
noahmmcgivern <noah.m.mcgivern@gmail.com>
2023-12-03 13:07:33 -0500

    feat: windows builds 5

--- src/bin/duroc_hog.rs
+++ src/bin/duroc_hog.rs
@@ -368,7 +368,7 @@ mod tests {
     }
 
     #[test]
-    #[cfg(not(target_os="windows"))]
+    #[cfg(not(target_os = "windows"))]
     fn does_not_scan_output_file() {
         let temp_dir = TempDir::new().unwrap();
 
